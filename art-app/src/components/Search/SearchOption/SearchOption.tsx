b8ff5dd373e3179500e2ab209b60ce49dfd4da84
dliang <humgal@outlook.com>
2023-04-12 17:23:43 +0800

    searchOption

--- art-app/src/components/Search/SearchOption/SearchOption.tsx
+++ art-app/src/components/Search/SearchOption/SearchOption.tsx
@@ -1,22 +1,86 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
+import style from "./SearchOption.css";
 
-import React from 'react'
-import { View,Text,Button} from 'react-native';
+const SearchOption = () => {
+  const [itemsColor, setItemColor] = useState("lightgrey");
+  const [gameColor, setGamecolor] = useState("lightgrey");
+  const [videoColor, setVideocolor] = useState("lightgrey");
+  const [animationColor, setAnimationColor] = useState("lightgrey");
+  const [photographyColor, setPhotographyColor] = useState("lightgrey");
 
+  function setDefaultColor() {
+    setItemColor("lightgrey");
+    setGamecolor("lightgrey");
+    setVideocolor("lightgrey");
+    setAnimationColor("lightgrey");
+    setPhotographyColor("lightgrey"); 
+  }
 
-const SearchOption = ()=>{
-    return(
-        <View style={{margin:5}}>
-        <Text> Types</Text>
-        <View  style={{flexDirection:'row', flexWrap: 'wrap' }} >
-        <Button title="All Item" />
-        <Button title = "Game" />
-        <Button title = "Video" />
-        <Button title = "Animation"  />
-        <Button title = "Photography" />
-        </View>
-        </View>
+  return (
+    <View style={{ margin: 5 }}>
+      <Text> Type</Text>
+      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            setDefaultColor()
+            setItemColor("mediumpurple");
+            
+          }}
+          style={[style.type, { backgroundColor: itemsColor }]}
+        >
+          All Items
+        </Button>
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            setDefaultColor()
+            setGamecolor("mediumpurple");
+           
+          }}
+          style={[style.type, { backgroundColor: gameColor }]}
+        >
+          Game
+        </Button>
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            setDefaultColor()
+            setVideocolor("mediumpurple");
+           
+          }}
+          style={[style.type, { backgroundColor: videoColor }]}
+        >
+          Video
+        </Button>
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            setDefaultColor()
+            setAnimationColor("mediumpurple");
+            
+          }}
+          style={[style.type, { backgroundColor: animationColor }]}
+        >
+          Animation
+        </Button>
 
-    )
-}
+        <Button
+          mode="contained-tonal"
+          onPress={() => {
+            setDefaultColor()
+            setPhotographyColor("mediumpurple");
+           
+          }}
+          style={[style.type, { backgroundColor: photographyColor }]}
+        >
+          Photography
+        </Button>
+      </View>
+    </View>
+  );
+};
 
 export default SearchOption;
